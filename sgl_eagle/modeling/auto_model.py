52074321090e2be4be3e10788cea82594e3c1ef7
yinfan98 <1106310035@qq.com>
2025-06-23 11:18:23 +0000

    fix loading

--- sgl_eagle/modeling/auto_model.py
+++ sgl_eagle/modeling/auto_model.py
@@ -1,11 +1,11 @@
 from transformers import AutoModelForCausalLM as AutoModelForCausalLMBase
 from transformers import LlamaConfig
 
-# from sgl_eagle.modeling.base.llama3.llama3 import LlamaForCausalLM
-from transformers.models.llama.modeling_llama import LlamaForCausalLM
-
+from sgl_eagle.modeling.base.llama3.llama3 import LlamaForCausalLM
 from sgl_eagle.modeling.base.llama4.llama4_17x16 import Llama4ForCausalLM
 from sgl_eagle.modeling.draft.llama3_eagle import LlamaForCausalLMEagle3
+
+# from transformers.models.llama.modeling_llama import LlamaForCausalLM
 
 
 class AutoModelForCausalLM(AutoModelForCausalLMBase):
